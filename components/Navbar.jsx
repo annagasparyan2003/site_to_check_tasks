b7f88f0f524182df6abed8edaf7a5e8eafbce226
annagasparyan2003 <an.anushik-gasparyan2003@mail.ru>
2024-04-19 22:00:46 +0300

    add new link for navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
      <a href=""> Publication report </a> 
      <a href=""> Главная </a>
      <a href=""> Публикации </a>
+     <a href="/send_work"> Отправить работу </a>
      <a href=""> <FontAwesomeIcon className='h-5' icon={faCircleUser} /> </a>
     </header>
   )
