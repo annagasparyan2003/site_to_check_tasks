569cdc5cd055fa9cf5beda78ef4f12c9faf1d518
annagasparyan2003 <an.anushik-gasparyan2003@mail.ru>
2024-05-04 01:08:33 +0300

    update send_work page and create publications page

--- app/send_work/page.jsx
+++ app/send_work/page.jsx
@@ -1,12 +1,73 @@
+"use client"
 import React from 'react'
+import "./send_work.css"
+
+const filetxtTypes = [
+  ".pdf",
+  ".doc",
+  ".docx",
+  "application/pdf",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  "application/msword"
+];
+
+function returnFileSize(number) {
+  if (number < 1024) {
+      return `${number} bytes`;
+  } else if (number >= 1024 && number < 1048576) {
+      return `${(number / 1024).toFixed(1)} KB`;
+  } else if (number >= 1048576) {
+      return `${(number / 1048576).toFixed(1)} MB`;
+  }
+}
+
+function validtxtFileType(file) {
+  console.log(file.type)
+  return filetxtTypes.includes(file.type);
+}
 
 const Send_work = () => {
+  
+  const changeFile = (e) => {
+    const text_input = e.currentTarget;
+    console.log(text_input)
+    const text_preview = document.querySelector(".text_preview")
+    while (text_preview.firstChild) {
+      text_preview.removeChild(text_preview.firstChild);
+    }
+    const curFiles = text_input.files;
+    if (curFiles.length === 0) {
+      const para = document.createElement("p");
+      para.textContent = "Нет выбранных файлов для загрузки";
+      text_preview.appendChild(para);
+  } else {
+      const list = document.createElement("ol");
+      text_preview.appendChild(list);
+
+      for (const file of curFiles) {
+          const listItem = document.createElement("li");
+          const para = document.createElement("p");
+          if (validtxtFileType(file)) {
+              para.innerHTML = ` Имя файла -  ${file.name}, \n Размер файла - ${returnFileSize(
+                  file.size,
+              )}.`;
+              para.className = "whitespace-pre-wrap" 
+              listItem.appendChild(para);
+          } else {
+              para.textContent = `Имя файла ${file.name}: Недопустимый тип файла. Замените файл.`;
+              listItem.appendChild(para);
+          }
+
+          list.appendChild(listItem);
+      }
+    }
+  }
   return (
     <>
-        <main className='flex'>
+        <main className='main_form'>
           <h1></h1>
-          <form action="" className='flex flex-col'>
-            <label htmlFor="fio_student">Фамилия Имя Отчество</label>
+          <form action="" className='forms_send_work'>
+            <label htmlFor="fio_student"> <p> Фамилия Имя Отчество </p> </label>
             <input type="text" name="fio_student" id="fio_student" />
 
             <label htmlFor="place_work_performers">Место работы исполнителей</label>
@@ -25,8 +86,8 @@ const Send_work = () => {
             <input type="text" className='lang_publication'/>
 
             <label htmlFor="type_publication">Тип отчета/ публикации</label>
-            <select name="type_publication" id='type_publication' size="1" className='border-none rounded-md focus:outline-none'>
-              <option selected value="s1">Отчет</option>
+            <select name="type_publication" id='type_publication' size="1" defaultValue={"s1"} className='choose border-none rounded-md focus:outline-none'>
+              <option value="s1">Отчет</option>
               <option value="s2">Тезис</option>
               <option value="s3">Статья</option>
               <option value="s4">Монография</option>
@@ -41,10 +102,16 @@ const Send_work = () => {
             <label htmlFor="output_data">Выходные данные, если имеются</label>
             <input type="text" className='output_data'/>
             
-            <label htmlFor="attach_publication"></label>
-            <input type="file" name="attach_publication" id="attach_publication" />
-
-            <input type="submit" value="Сохранить" />
+            <div className='file_input'>
+            <label htmlFor="attach_publication">Выберите файл для загрузки (PDF, DOC, DOCX)</label>
+            <input className='hidden' type="file" onChange={changeFile} name="attach_publication" id="attach_publication"
+                accept=".doc,.docx,.pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document">
+                  </input>
+        </div>
+        <div className="text_preview">
+            <p>Нет файлов, выбранных в настоящий момент</p>
+        </div>
+            <input type="submit" defaultValue="Сохранить" />
             
             
             
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <header className='h-10 w-screen flex justify-between'>
-     <a href=""> Publication report </a> 
-     <a href=""> Главная </a>
-     <a href=""> Публикации </a>
-     <a href="/send_work"> Отправить работу </a>
-     <a href=""> <FontAwesomeIcon className='h-5' icon={faCircleUser} /> </a>
+     <Link href=""> Publication report </Link> 
+     <Link href=""> Главная </Link>
+     <Link href="/publications"> Публикации </Link>
+     <Link href="/send_work"> Отправить работу </Link>
+     <Link href=""> <FontAwesomeIcon className='h-5 pr-2' icon={faCircleUser} /> </Link>
     </header>
   )
 }
